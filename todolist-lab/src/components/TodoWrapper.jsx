fb5b8f153f4e47a5343379b2c862fd479d4ed257
Júlia Borges <juliaborgesfacul@gmail.com>
2024-06-01 19:09:40 -0300

    React implementado com sucesso!

---
+++ todolist-lab/src/components/TodoWrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TodoForm } from './TodoForm';
+import { TodoList } from './TodoList';
+export const TodoWrapper = () => {
+  return (
+    <div className='TodoWrapper'>
+      <h1>Lista de Tarefas</h1>
+      <TodoForm />
+      <TodoList/>
+    </div>
+  );
+};
